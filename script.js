77c22c8cf11c4909f40752481a025f215ace6f15
eriksvetlik <svetlik.erik@gmail.com>
2022-01-09 12:02:05 -0600

    added local storage functionality

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var currentDay = $("#currentDay");
 var currentHour = moment().hour();
 var hourBlock = $(".hourBlock");
+var hourID;
+var saveBtn = $(".saveBtn");
+var task = $(".task");
 
-// function to display the date with Moment.js
-function displayDay() {
-  $(currentDay).text(moment().format("h:mm a | dddd, MMMM D | YYYY"));
+// function to bring values from local storage and display on page
+function taskStorage() {
+  $(hourBlock).each(function () {
+    hourID = $(this).attr("id");
+
+    var getTask = localStorage.getItem(hourID);
+    $(this).children().find(task).val(getTask);
+  });
 }
 
 // function to color each time block if the time block is behind, equal to, or ahead of the current time
@@ -22,5 +30,19 @@ function colorHourBlocks() {
   });
 }
 
+// function to display the date with Moment.js
+function displayDay() {
+  $(currentDay).text(moment().format("h:mm a | dddd, MMMM D | YYYY"));
+}
+
+// when the Save button is clicked, the hourBlock ID and task content are saved
+$(saveBtn).click(function () {
+  var taskContent = $(this).parent().siblings().find(task).val();
+  var id = $(this).parent().parent().attr("id");
+
+  localStorage.setItem(id, taskContent);
+});
+
 colorHourBlocks();
 displayDay();
+taskStorage();
